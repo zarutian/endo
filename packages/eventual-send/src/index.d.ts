2ab4eb3ed798dc491e6f57609d3719435571dabd
Michael FIG <mfig@agoric.com>
2022-06-03 15:17:19 -0600

    feat(captp)!: implement garbage collection

--- packages/eventual-send/src/index.d.ts
+++ packages/eventual-send/src/index.d.ts
@@ -135,7 +135,7 @@ export type ResolveWithPresenceOptionsBag<T extends Object> = {
   };
 };
 
-export type HandledExecutor<R> = (
+export type HandledExecutor<R = unknown> = (
   resolveHandled: (value?: R) => void,
   rejectHandled: (reason?: unknown) => void,
   resolveWithPresence: (
@@ -143,6 +143,20 @@ export type HandledExecutor<R> = (
     options?: ResolveWithPresenceOptionsBag<{}>,
   ) => object,
 ) => void;
+
+declare interface Settler<R = unknown> {
+  resolve: (value?: R) => void;
+  reject: (reason?: unknown) => void;
+  resolveWithPresence: (
+    presenceHandler?: EHandler<{}>,
+    options?: ResolveWithPresenceOptionsBag<{}>,
+  ) => object;
+}
+
+declare interface RemoteKit<R = unknown> {
+  promise: Promise<R>;
+  settler: Settler<R>;
+}
 
 declare interface HandledPromiseStaticMethods {
   applyFunction(target: unknown, args: unknown[]): Promise<unknown>;
